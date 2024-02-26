2a9219ff94594fe3df4eda92a7fbe95a0b2a724f
YasmiinLDN <yasmin.a.ibrahim@icloud.com>
2024-02-26 01:27:24 +0000

    added css

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -11,7 +11,7 @@ function Contact() {
                 <i className="fas fa-map-marked-alt text-primary mb-2"></i>
                 <h3 className="text-uppercase m-0 fw-bolder">Address</h3>
                 <hr className="my-4" />
-                <div>London, United Kingdom </div>
+                <div className="text-light">London, United Kingdom </div>
               </div>
             </div>
           </div>
@@ -22,7 +22,7 @@ function Contact() {
                 <h3 className="text-uppercase m-0 fw-bolder">Email</h3>
                 <hr className="my-4" />
                 <div>
-                  <a href="mailto:yasmin.a.ibrahim@icloud.com">yasmin.project@icloud.com</a>
+                  <a className="text-light" href="mailto:yasmin.a.ibrahim@icloud.com">yasmin.project@icloud.com</a>
                 </div>
               </div>
             </div>
@@ -31,9 +31,9 @@ function Contact() {
             <div className="card py-4 h-100 bg-light">
               <div className="card-body text-center">
                 <i className="fas fa-mobile-alt text-primary mb-2"></i>
-                <h3 className="text-uppercase m-0 fw-bolder">Phone</h3>
+                <h3 className="text-uppercase m-0 fw-bolder text-light">Phone</h3>
                 <hr className="my-4" />
-                <a href="077 1234 5678">077 1234 5678</a>
+                <a className="text-light" href="077 1234 5678">077 1234 5678</a>
               </div>
             </div>
           </div>
