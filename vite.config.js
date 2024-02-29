89422ff50829635725c5895e9fbae9d0dd4c4390
YasmiinLDN <yasmin.a.ibrahim@icloud.com>
2024-02-29 15:42:07 +0000

    trying to deploy again with different package and package-lock .json file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-Portfolio-Yasmiin',
+  build: {
+    outDir: 'build'
+  },
   plugins: [react()],
 })
